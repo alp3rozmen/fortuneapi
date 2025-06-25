96cc42b2b57d3467ad55ef80feb3e999ef0d321d
alp3rozmen <alp3rozmen@gmail.com>
2025-06-25 21:29:02 +0300

    all files

---
+++ knex/connection.js
@@ -0,0 +1,20 @@
+import knex from 'knex'
+
+const connection = knex({
+    client: 'mysql',
+    connection: {
+        host : '127.0.0.1',
+        user : 'root',
+        password : 'example',
+        database : 'fortuneteller'
+    },
+    migrations: {
+        tableName: 'migrations',
+        directory: './migrations'
+    },
+    seeds: {
+        directory: './seeds'
+    }
+})
+
+export default connection
